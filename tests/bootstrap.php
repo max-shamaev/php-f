9889e38299c28fb813f7d0a1139580766e2c5ad1
Maxim Shamaev <max@cdev.ru>
2011-09-03 06:29:24 +0000

    [*] Tests core added

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+/**
+ * PHP version 5.3.0
+ * 
+ * @author  Maxim Shamaev (maxim.shamaev@gmail.com) 
+ * @license Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ * @version $id$
+ * @link    https://github.com/max-shamaev/php-f
+ * @since   1.0.0
+ */
+
+@date_default_timezone_set(@date_default_timezone_get());
+
+require_once __DIR__ . '/../src/PHPF.php';
+
